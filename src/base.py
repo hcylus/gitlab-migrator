bb41e0e4f6bd08e74fae17b8307dfa5720fff7af
luhaichuan <luhaichuan@luhaichuan.com>
2020-03-01 14:12:15 +0800

    rename cache to base,and add apiOperate function

--- src/base.py
+++ src/base.py
@@ -1,11 +1,24 @@
 # -*- coding: utf-8 -*-
 
-import json,os
+import json,os,requests
 
-def storage(name, data):
-	cachepath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'cache')
-	if not os.path.exists(cachepath):
-		os.makedirs(cachepath)
-		
-	with open(cachepath + '/%s.json' % name, 'w', encoding = 'UTF-8') as f:
-		json.dump(data, f, sort_keys = False, indent = 2, ensure_ascii = False)
+class BaseApi(object):
+	def __init__(self):
+		super(BaseApi, self).__init__()
+
+	def cache(self, name, data):
+		cachepath = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))),'cache')
+		if not os.path.exists(cachepath):
+			os.makedirs(cachepath)
+			
+		with open(cachepath + '/%s.json' % name, 'w', encoding = 'UTF-8') as f:
+			json.dump(data, f, sort_keys = False, indent = 2, ensure_ascii = False)
+
+	def apiOperate(self, api,config,**params):
+		param = { 'per_page': config['per_page'], 'sort': 'asc' ,'order_by': 'id'}
+		param.update(params)
+		print(param)
+		resp = requests.get(api % config['address'],
+			headers = config['headers'], params = param)
+
+		return resp
